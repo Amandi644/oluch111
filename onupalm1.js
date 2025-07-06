d0b97549cff4208698bb029c7d3dd01b29827162
Amandi644 <okukugeorgi@yahoo.com>
2025-07-06 16:15:38 +0200

    Add files via upload

---
+++ onupalm1.js
@@ -0,0 +1,23 @@
+// I use ImageTime TO CALL A function!
+const imageTime = document.getElementById("imageTimes");
+function imageTimes(){
+alert(`Items (${1 + 1}): $${(12000 + 15000/ 100)}`)
+}
+imageTimes();
+
+
+
+let buttons = document.getElementById("buttons");
+addEventListener("click",function(buttons){
+
+buttons = "";
+
+if (true < 21){ 
+alert( "Welcome our Customers " );
+console.log("can read my message !");
+}else if(false > 23){
+console.log("answer me that he is not",24);
+} else if(true> 65){
+console.log("i was not ",24);
+}
+});
